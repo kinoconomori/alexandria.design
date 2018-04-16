0e1fdd0b917765ad35824c0dc90faaf8e94451af
kinoconomori <atroup@cca.edu>
2018-04-15 21:58:30 -0700

    Hi alli
    troup

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+$( function() {
+   $( ".drag1" ).draggable({ containment: ".play", scroll: false });
+    $( ".drag2" ).draggable({ containment: "parent" });
+    $( ".drag3" ).draggable({ containment: "parent" });
+    $( ".drag4" ).draggable({ containment: "parent" });
+    $( ".drag5" ).draggable({ containment: "parent" });
+    $( ".drag6" ).draggable({ containment: "parent" });
+    $( ".drag7" ).draggable({ containment: "parent" });
+    $( ".drag8" ).draggable({ containment: "parent" });
+    $( ".drag9" ).draggable({ containment: "parent" });
+    $( ".drag10" ).draggable({ containment: "parent" });
+    $( ".drag11" ).draggable({ containment: "parent" });
+  } );
+
+$(document).ready(function() {
+            var colors = ["#f0492f", "#f1bd2b", "#74cdd7", "#27a775"];
+            var rand = Math.floor(Math.random() * colors.length);
+            $('.profile').css("background-color", colors[rand]);
+});
+
