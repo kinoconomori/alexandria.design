e572f5cfc7dbf0727e02905189bea6c1eb85771b
kinoconomori <atroup@cca.edu>
2021-02-19 15:29:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 $( function() {
    $( ".drag1" ).draggable({ containment: ".play", scroll: false });
     $( ".drag2" ).draggable({ containment: "parent" });
@@ -19,24 +21,19 @@ $( function() {
 
 
 
-$(function() {
-    //----- OPEN
-    $('[data-popup-open]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-open');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
- 
-        e.preventDefault();
-    });
- 
-    //----- CLOSE
-    $('[data-popup-close]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-close');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
- 
-        e.preventDefault();
-    });
+
+
+$(document).ready(function() {
+            var colors = ["#ee4b38", "#31a676", "#77cdd7", "#f1bc3e"];
+            var rand = Math.floor(Math.random() * colors.length);
+            $('.profilehead').css("color", colors[rand]);
+            $('.overlay').css("background-color", colors[rand]);
 });
 
-
-
-
+$(document).ready(function() {
+            var colors = ["#ee4b38", "#31a676", "#77cdd7", "#f1bc3e"];
+            var rand = Math.floor(Math.random() * colors.length);
+            $('.mainhead').css("color", colors[rand]);
+            $('.overlay').css("background-color", colors[rand]);
+});
+  
